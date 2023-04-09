b0a3f0ae31f37e61f4a710235ae679d93dd984bf
Leo Bi <biminglei@gmail.com>
2023-04-09 20:52:29 +0800

    update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
 
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  heroes : Hero[] = [];
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   hero: Hero = {
     id: 1,
